929ae18a49faeb690ccf42bc13bb0b6e000af00d
animationcreator38 <147336591+animationcreator38@users.noreply.github.com>
2025-03-06 08:40:57 -0800

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2189,23 +2189,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )
     elif query.data == "shortlink_info":
-        btn = [[
-            InlineKeyboardButton("👇Select Your Language 👇", callback_data="laninfo")
-        ],[
-            InlineKeyboardButton("Tamil", callback_data="tamil_info"),
-            InlineKeyboardButton("English", callback_data="english_info"),
-            InlineKeyboardButton("Hindi", callback_data="hindi_info")
-        ],[
-            InlineKeyboardButton("Malayalam", callback_data="malayalam_info"),
-            InlineKeyboardButton("Urdu", callback_data="urdu_info"),
-            InlineKeyboardButton("Bangla", callback_data="bangladesh_info")
-        ],[
-            InlineKeyboardButton("Telugu", callback_data="telugu_info"),
-            InlineKeyboardButton("Kannada", callback_data="kannada_info"),
-            InlineKeyboardButton("Gujarati", callback_data="gujarati_info")
-        ],[
-            InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start")
-        ]]
+        btn = [[],[
+            InlineKeyboardButton("📞 Cᴏɴᴛᴀᴄᴛ", url="telegram.me/cinewood_1"),
+            InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
+        ],[]]
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
